c511c2b402ea8bf0aea07f6bf767e8f2a4d61f1e
karolex72 <71222315+karolex72@users.noreply.github.com>
2022-10-07 22:44:51 +0200

    Updated project

---
+++ chatbot-master/constants.js
@@ -0,0 +1,99 @@
+// Options the user could type in
+const prompts = [
+  ["student no", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+  ["Which courses are offered by the FCI"]
+  ["apply"]
+  ["requirements", "Informatics"]
+  ["requirements", "Computer", "Science"]
+  ["requirements", "Cyber", "Security"]
+  ["tuition"]}
+
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+  ["Bachelor of Informatics
+    "Bachelor of Informatics Honours
+    "Postgraduate Certificate in Informatics
+    "Master of Informatics
+    "Doctor of Philosophy in Informatics
+
+    "Bachelor of Computer Science
+    "Bachelor of Computer Science in Cyber Security
+    "Bachelor of Computer Science Honours
+    "Master of Computer Science
+    "Doctor of Philosophy in Computer Science"]
+  ["Please visit: admission.nust.na/FFCI.html"]
+  ["Admission requires a D-symbol in Mathematics on NSSC(O), in addition to meeting general admission requirements of NUST"]
+  ["General admission requirements of NUST including a D-symbol in Mathematics on NSSC(O)"]
+  ["Cyber Security admission requires a D-symbol in Mathematics on NSSC(O), in addition to meeting general admission requirements of NUST"]
+  ["You can find tuition fees here: https://www.nust.na/sites/default/files/documents/Guide-to-Prospective-Students-2023.pdf"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
